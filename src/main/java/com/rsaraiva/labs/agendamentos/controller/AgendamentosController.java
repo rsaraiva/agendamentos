6a41ca68fbcdac5a4fc01ead96f5ab055357bf8b
Rubens Saraiva <rubens.saraiva@gmail.com>
2014-09-01 21:59:19 -0300

    iniciando projeto web com vraptor4

---
+++ src/main/java/com/rsaraiva/labs/agendamentos/controller/AgendamentosController.java
@@ -0,0 +1,11 @@
+package com.rsaraiva.labs.agendamentos.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class AgendamentosController {
+
+    @Path("/")
+    public void agendamentos() {}
+}
